7aa18d48b19b82e670aef6d676f2cc9c84f9ee7e
HsnGoodDev <103339786+HsnGoodDev@users.noreply.github.com>
2022-05-02 09:07:52 +0900

    Time: 2 ms (63.25%), Space: 41.9 MB (18.85%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int reverse(int x) {
+        // int to string 
+        // - 기호 경우 유지 
+        // reverse 후 0이 첫 숫자 면 생략 
+        int resultNumber = 0;
+        while (x != 0)
+    {
+        int tail = x % 10;
+        int newResult = resultNumber * 10 + tail;
+        if ((newResult - tail) / 10 != resultNumber)
+        { return 0; }
+        resultNumber = newResult;
+        x = x / 10;
+    }
+
+    return resultNumber;
+    }
+}
